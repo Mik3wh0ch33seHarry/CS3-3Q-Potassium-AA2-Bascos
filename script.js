e2868c623cb4d92e9bdca01754d82ed6f0a554d7
Mik3wh0ch33seHarry <khtbascos22@src.pshs.edu.ph>
2025-02-12 15:46:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@
          let monsterAndlocation = location.concat("", monster).toUpperCase();
 
          
-         document.getElementById("result").innerHTML=" After " + timeHours + "hour/s, the population of " + nameAndlocation + "has risend to " + finalPopulation; 
+         document.getElementById("result").innerHTML=" After " + timeHours + "hour/s, the population of " + monsterAndlocation + "has risend to " + finalPopulation; 
       
       }
      
